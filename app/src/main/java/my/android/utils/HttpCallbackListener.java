e9d2386fa6c576c238a69fd3692a59463776177d
jiangjianqing <cz_jjq@163.com>
2015-11-13 13:55:52 +0800

    添加HttpUtil

---
+++ app/src/main/java/my/android/utils/HttpCallbackListener.java
@@ -0,0 +1,19 @@
+package my.android.utils;
+
+/**
+ * HttpUtil使用的回调接口
+ * Created by ztxs on 15-11-13.
+ */
+public interface HttpCallbackListener {
+    /**
+     * 当服务器成功响应我们请求的时候调用
+     * @param response
+     */
+    void onFinish(String response);
+
+    /**
+     * 当进行网络操作出现错误的时候调用
+     * @param e
+     */
+    void onError(Exception e);
+}
